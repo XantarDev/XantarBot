2fb35bb114fcaf3cb66e67d016d89cd754cb22c7
David Gonzalo <dagope@gmail.com>
2021-04-10 16:42:43 +0200

    clean code

--- Program.cs
+++ Program.cs
@@ -36,13 +36,6 @@ namespace XantarBot
             var CONSUMER_SECRET = args.ElementAtOrDefault(i++) ?? _CONSUMER_SECRET; 
             var ACCESS_TOKEN = args.ElementAtOrDefault(i++) ?? _ACCESS_TOKEN; 
             var ACCESS_SECRET = args.ElementAtOrDefault(i++) ?? _ACCESS_SECRET; 
-
-            Console.WriteLine($"");
-            Console.WriteLine($"CONSUMER_KEY: {CONSUMER_KEY.Substring(0,5)}");
-            Console.WriteLine($"CONSUMER_SECRET: {CONSUMER_SECRET.Substring(0,5)}");
-            Console.WriteLine($"ACCESS_TOKEN: {ACCESS_TOKEN.Substring(0,5)}");
-            Console.WriteLine($"ACCESS_SECRET: {ACCESS_SECRET.Substring(0,5)}");
-            Console.WriteLine($"");
             
             var client = new TwitterClient(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_SECRET);
             var user = client.Users.GetAuthenticatedUserAsync().Result;
@@ -50,18 +43,18 @@ namespace XantarBot
             var lastUpdate = Regex.Match(user.GetHomeTimelineAsync().Result.FirstOrDefault().Text, @"\[(.*?)\]").Groups[1].Value;
             var vaccination = GetVaccination();
             
-            //if (Convert.ToDateTime(lastUpdate, new CultureInfo("es-ES")) < vaccination.Date)
-            //{
+            if (Convert.ToDateTime(lastUpdate, new CultureInfo("es-ES")) < vaccination.Date)
+            {
                 var progressBar = new string(FULL, (int)vaccination.Percentage / 5) + new string(EMPTY, 20 - ((int)vaccination.Percentage / 5));
                 var textTweet = $"[{vaccination.Date.ToString("dd/MM/yyyy")}] {vaccination.Percentage}%\n{progressBar}";
                 _ = user.PublishTweetAsync(textTweet).Result;
 
                 Console.WriteLine($"Tweet published: {textTweet}");
-            //}
-            //else
-            //{
-            //    Console.WriteLine($"No updated data. Try again later.");
-            //}
+            }
+            else
+            {
+                Console.WriteLine($"No updated data. Try again later.");
+            }
         }
 
         private static Vaccination GetVaccination()
